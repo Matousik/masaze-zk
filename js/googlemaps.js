f6fd3f94e07bb2edc8f858a08fbd357f7adcf3c6
Matousik <matousik@gmail.com>
2022-04-11 00:01:28 +0200

    done autopatie a tcm

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -4,28 +4,32 @@ let map2;
 
 function initMap() {
 
-    const LatLng1 = { lat: 50.661, lng: 14.044 };
-    const LatLng2 = { lat: -34.397, lng: 150.644 };
-
-    map1 = new google.maps.Map(document.getElementById("map1"), {
-        center: { lat: 50.661, lng: 14.044 },
-        zoom: 8,
-        })
-
-    new google.maps.Marker({
-        position: LatLng1,
-        map1,
-        title: "Provozovna Libochovice",
-        });
+    const LatLng1 = { lat: 50.40578799835035, lng: 14.03988639465556 };
+    const LatLng2 = { lat: 50.662074161492015, lng: 14.044625078796344 };
   
     map2 = new google.maps.Map(document.getElementById("map2"), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
+        center: LatLng2,
+        zoom: 15,
         });
 
-    new google.maps.Marker({
+    var marker1 = new google.maps.Marker({
         position: LatLng2,
         map2,
         title: "Provozovna Ústí nad Labem",
         });
+    
+        marker1.setMap(map2);
+
+    map1 = new google.maps.Map(document.getElementById("map1"), {
+        center: LatLng1,
+        zoom: 15,
+        });
+
+    var marker2 = new google.maps.Marker({
+        position: LatLng1,
+        map1,
+        title: "Provozovna Libochovice",
+        });    
+
+        marker2.setMap(map1);
 }
