f73d5605061889c94983be5243223eecf29d2cdc
Matousik <matousik@gmail.com>
2022-04-13 19:12:39 +0200

    corrected sidebar x overflow, corrected map width

--- js/base.js
+++ js/base.js
@@ -5,11 +5,23 @@ const navlinks = navlinksarr[0];
 const closingIcon = document.getElementById('menuOffIcon');
 const openingIcon = document.getElementById('menuIcon');
 
+let timeout;
+
 openingIcon.addEventListener('click', function() {
-    navlinks.style.right= '0';
+    navlinks.style.display='block';
+    timeout = setTimeout(slideIn, 20);
+
 })
 
 closingIcon.addEventListener('click', function() {
     navlinks.style.right= "-200px";
+    timeout = setTimeout(slideOutTerminator, 1000);
 })
 
+function slideIn() {
+    navlinks.style.right= '0';
+}
+
+function slideOutTerminator() {
+    navlinks.style.display='none';
+}
